f1d98be1acb842a82c0be7498e3405202efe8cab
ItzSelenux <ItzSelenux@zariep.com>
2024-07-11 14:48:02 -0600

    fix

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 all: AppImage
 
 AppImage:
-	create-appimage.sh
+	./create-appimage.sh
+
+test:
+	./GTK_Pipe_Viewer-x86_64.AppImage
 
 clean:
-	rm -rf pipe-viewer-main* tmplibs
+	rm -rf pipe-viewer-main*
--- create-appimage.sh
+++ create-appimage.sh
@@ -9,21 +9,24 @@ fi
 wget https://github.com/trizen/pipe-viewer/archive/main.zip -O pipe-viewer-main.zip &&
 unzip -n pipe-viewer-main.zip &&
 
-cp AppRun pipe-viewer-main &&
-cp gtk-pipe-viewer.png pipe-viewer-main &&
-cp gtk-pipe-viewer.desktop pipe-viewer-main &&
-cp left_arrow.png pipe-viewer-main/share/icons/ &&
-cp right_arrow.png pipe-viewer-main/share/icons/ &&
-cp list-remove-symbolic.svg pipe-viewer-main/share/icons/ &&
+cp -v AppRun pipe-viewer-main &&
+cp -v gtk-pipe-viewer.png pipe-viewer-main &&
+cp -v gtk-pipe-viewer.desktop pipe-viewer-main &&
+cp -v left_arrow.png pipe-viewer-main/share/icons/ &&
+cp -v right_arrow.png pipe-viewer-main/share/icons/ &&
+cp -v list-remove-symbolic.svg pipe-viewer-main/share/icons/ &&
 
+cpanm --local-lib=pipe-viewer-main Cairo::GObject Glib::Object::Introspection LWP::Protocol::https Data::Dump JSON Gtk3 File::ShareDir &&
 cd pipe-viewer-main &&
 
-cpanm --local-lib=. LWP::Protocol::https Data::Dump JSON Gtk3 File::ShareDir &&
-mv lib/WWW lib/perl5/WWW &&
+mkdir -pv lib/perl5/WWW
+cp -vr lib/WWW/* lib/perl5/WWW &&
 
 sed -i 's/my \$glade_file = catfile(\$share_dir, "gtk-pipe-viewer.glade");/use FindBin;\nmy \$dir = \$FindBin::Bin;\nmy \$glade_file = catfile(\$dir,"..","share", "gtk-pipe-viewer.glade");/' "bin/gtk-pipe-viewer" &&
 sed -i 's/my \$icons_path = catdir(\$share_dir, '\''icons'\'');/my \$icons_path = catdir(\$dir, "..","share", '\''icons'\'');/' "bin/gtk-pipe-viewer" &&
 sed -i '/my $share_dir =/,/dist_dir/d' bin/gtk-pipe-viewer &&
 
+chmod +x AppRun &&
+
 cd ../ &&
-ARCH=x86_64 appimagetool-x86_64.AppImage pipe-viewer-main/
+ARCH=x86_64 ./appimagetool-x86_64.AppImage pipe-viewer-main/
